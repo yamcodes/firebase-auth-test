ba10f2fd1389e4e7a87245ce35c2fd125f3a3641
Yam Borodetsky <hi@yam.codes>
2024-04-24 20:21:16 +0500

    fix: auth file structure

--- packages/fat-auth/src/react/components/logged-in.tsx
+++ packages/fat-auth/src/react/components/logged-in.tsx
@@ -3,9 +3,6 @@ import { useFatAuth } from '../contexts';
 
 export const LoggedIn = ({ children }: PropsWithChildren) => {
   const { user } = useFatAuth();
-
-  console.log('Checking if logged in');
   if (!user) return null;
-  console.log('LoggedIn', user);
   return <>{children}</>;
 };
--- packages/fat-auth/tsup.config.ts
+++ packages/fat-auth/tsup.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'tsup';
 // eslint-disable-next-line import/no-default-export -- tsup config
 export default defineConfig([
   {
-    entry: ['core/index.ts'],
-    outDir: 'core/dist',
+    entry: {
+      core: 'src/core/index.ts',
+      react: 'src/react/index.ts',
+    },
+    outDir: 'dist',
     splitting: false,
     treeshake: true,
     minify: true,
@@ -15,21 +18,6 @@ export default defineConfig([
     shims: true,
     injectStyle: false,
     external: ['react', 'react-dom'],
-    noExternal: ['hooks'],
-  },
-  {
-    entry: ['react/index.ts'],
-    outDir: 'react/dist',
-    splitting: false,
-    treeshake: true,
-    minify: true,
-    dts: true,
-    sourcemap: 'inline',
-    clean: false,
-    format: ['cjs', 'esm'],
-    shims: true,
-    injectStyle: false,
-    external: ['react', 'react-dom'],
-    noExternal: ['hooks'],
+    noExternal: ['hooks', 'utilities'],
   },
 ]);
