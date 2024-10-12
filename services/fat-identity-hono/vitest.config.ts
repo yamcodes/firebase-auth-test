6230b632c8c64e6127c56f727618f165c50e9e53
Yam Borodetsky <hi@yam.codes>
2024-10-13 02:29:47 +0500

    refactor: format

--- services/fat-identity-hono/vitest.config.ts
+++ services/fat-identity-hono/vitest.config.ts
@@ -1,5 +1,3 @@
-import { ValidateEnv as validateEnv } from "@julr/vite-plugin-validate-env";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig, mergeConfig } from "vitest/config";
 import { sharedConfig } from "./vite.config";
 
