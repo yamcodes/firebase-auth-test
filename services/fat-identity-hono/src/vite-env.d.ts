f3ed319c0ccaba9e9433ea28570aab2c31452098
Yam Borodetsky <hi@yam.codes>
2024-10-23 01:11:43 +0500

    refactor: format

--- services/fat-identity-hono/src/vite-env.d.ts
+++ services/fat-identity-hono/src/vite-env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
-type ImportMetaEnvAugmented =
-	import("@julr/vite-plugin-validate-env").ImportMetaEnvAugmented<
-		typeof import("../env.config").default
-	>;
+type ImportMetaEnvAugmented = import(
+	"@julr/vite-plugin-validate-env",
+).ImportMetaEnvAugmented<typeof import("../env.config").default>;
 
 interface ImportMetaEnv extends ImportMetaEnvAugmented {}
