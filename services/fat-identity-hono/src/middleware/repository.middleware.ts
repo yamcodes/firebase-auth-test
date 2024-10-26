f4075340ebcf0b9c3326d5544d968fea9f248447
Yam Borodetsky <hi@yam.codes>
2024-10-27 00:30:55 +0500

    chore(identity-hono): remove unused code

--- services/fat-identity-hono/src/middleware/repository.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { createMiddleware } from "hono/factory";
-import { Container } from "~/di/container";
-
-export const injectRepositories = createMiddleware(async (c, next) => {
-	const container = Container.getInstance();
-	c.set("greetingsRepository", container.getGreetingsRepository());
-	await next();
-});
